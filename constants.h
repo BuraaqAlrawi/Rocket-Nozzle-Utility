e1fa1b24d290e7d5683806713200407a2bd6f529
test <balrawi@ucmerced.edu>
2019-01-16 23:12:57 -0800

    first commit

---
+++ constants.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+namespace constants
+{
+	constexpr double pi = 3.14159;
+	constexpr double avogadro = 60.0221413e23;
+	constexpr double gravity = 9.81;
+	constexpr double R = 8.3144598;
+	constexpr double sealevelpressure = 101325;
+	constexpr double KNitrogen = 1.4;
+
+}
+
+#endif
